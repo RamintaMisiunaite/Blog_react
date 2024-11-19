5c439d0ba0e6214bbf183d628a3833efe1308388
RamintaMis <raminta.misiunaite@gmail.com>
2024-11-19 16:40:58 +0200

    added autocomplete for submit form

--- src/components/create.js
+++ src/components/create.js
@@ -6,6 +6,8 @@ import moment from 'moment';
 const db = fb.firestore()
 const Blogs = db.collection('blogs');
 
+const possibleValues = ['Knygos', 'Fuck Diet']; 
+
 
 const CreateBlog= () => {
     const [title , SetTitle] = useState("");
@@ -30,23 +32,70 @@ const CreateBlog= () => {
             console.error("Error adding document: ", error);
         });
     }
+
+    // try auto complete
+    const [inputValue, setInputValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+
+
+    const handleInputChange = (event) => {
+        const theme = event.target.value;
+        SetTheme(theme);
+
+        if (theme.length > 0 ){
+            const filteredSuggestions = possibleValues.filter(suggestion =>
+                suggestion.toLowerCase().includes(theme.toLowerCase())
+            );
+            setSuggestions(filteredSuggestions);
+        }else{
+            setSuggestions([]);
+        }
+    }
+
+    const handleSuggestionClick = (theme) => {
+        SetTheme(theme);
+        setSuggestions([]);
+      };
+    
+    const clearThemeonClick = () => {
+        SetTheme('');
+    }
+
     return (
+    <>
+    <h1>Create a new blog entry</h1>
     <div class='blog-placeholder'>
-        <form onSubmit={(event) => {sub(event)}}>    
-            <input type="text" placeholder="Title" 
+        <form className='submit-form' onSubmit={(event) => {sub(event)}}>    
+            <input className='form-input-title' type="text" placeholder="Title" 
             onChange={(e)=>{SetTitle(e.target.value)}} required />
             
-            <input type="text" placeholder="Theme" 
-            onChange={(e)=>{SetTheme(e.target.value)}} required />
+{/* 
+            <input className='form-input-theme' type="text" placeholder="Theme" 
+            onChange={(e)=>{SetTheme(e.target.value)}} required /> */}
 
-            <textarea  name="content" type="text" placeholder="write your content here" 
-            rows="10" cols="150" onChange={(e)=>{SetBody(e.target.value)}} required >
+            <input className='form-input-theme' type="text" placeholder="Theme" value={theme}
+            onChange={handleInputChange} required />{suggestions.length > 0 && (
+                <ul className="suggestions-list">
+                  {suggestions.map((suggestion, index) => (
+                    <li
+                    key={index}
+                    onClick={() => handleSuggestionClick(suggestion)}
+                    // Additional props
+                    >
+                    {suggestion}
+                    </li>
+                ))}
+                </ul> 
+              )}                
+
+            <textarea className='form-input-text'  name="content" type="text" placeholder="write your text here" 
+            rows="30" cols="150" onChange={(e)=>{SetBody(e.target.value)}} required >
             </textarea>
 
-            <button type="submit">Submit</button>
+            <button className='submit-btn' type="submit">Submit</button>
         </form>
     </div>
-
+    </>
     );
 }
 
